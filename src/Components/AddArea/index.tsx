a4a2139b610cc7313f49c0f6f7258d42fb586b88
Diego Rocha <dkxever@gmail.com>
2022-02-16 18:24:58 -0300

    Creating handleKeyUp function and input states

--- src/Components/AddArea/index.tsx
+++ src/Components/AddArea/index.tsx
@@ -1,3 +1,5 @@
+import { useState, KeyboardEvent } from "react";
+
 type Props = {
   onEnter: (taskName: string) => void;
 }
@@ -6,12 +8,34 @@ type Props = {
 
 const AddArea = ( { onEnter }: Props ) => {
 
+  const [ inputText, setInputText ] = useState('');
+  const [ inputWait, setInputWait ] = useState(false);
+
+  // Input submit através do teclado
+
+  const handleKeyUp = ( e: KeyboardEvent ) => {
+    /* verifica a tecla e o valor contido no inputText */
+    if(e.code === 'Enter' && inputText !== ''){
+      onEnter(inputText);
+      setInputText('');
+      /* tempo de espera, para conseguior adicionar outra tarefa */
+      setInputWait(true);
+      setTimeout(() => {
+        setInputWait(false);
+      },1000);
+    }
+  }
+
   return (
     <C.Container>
       <div className="image">➕</div>
       <input
         type="text"
+        value={inputText}
+        onChange={({target}) => setInputText(target.value)}
         placeholder="Adicione alguma tarefa"
+        onKeyUp={handleKeyUp}
+        disabled={inputWait}
       />
     </C.Container>
   )
